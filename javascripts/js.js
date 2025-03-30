eaaa17ac6072494f316ff8759fd692cba25638a4
misserra <misserra17@gmail.com>
2025-03-30 19:14:45 +0300

    господи дай мне сил

--- javascripts/js.js
+++ javascripts/js.js
@@ -1,94 +1,169 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-  animIMG()
-    const btn = document.getElementById("btn");
-    btn.addEventListener('click', function() {
-      btn.classList.remove('buttonAnim');
-      void btn.offsetWidth;
-      btn.classList.remove('button');
-      btn.classList.add('buttonAnim'); 
-      btn.addEventListener('animationend', function() {
+    animIMG()
+      const btn = document.getElementById("btn");
+      btn.addEventListener('click', function() {
         btn.classList.remove('buttonAnim');
-        btn.classList.add('button');
-      })
-    });
-  
-
-  
-  const buttonChoise = document.querySelector('.choiseButton');
-  const choiseimg = document.querySelector('.choiseimg');
-  
-  if (buttonChoise && choiseimg) {
-    buttonChoise.addEventListener('click', function(e) {
-      e.preventDefault();
-      e.stopPropagation();
-      choiseimg.classList.toggle('talisman');
+        void btn.offsetWidth;
+        btn.classList.remove('button');
+        btn.classList.add('buttonAnim'); 
+        btn.addEventListener('animationend', function() {
+          btn.classList.remove('buttonAnim');
+          btn.classList.add('button');
+        })
       });
-  }
+    
+    const buttonChoise = document.querySelector('.choiseButton');
+    const choiseimg = document.querySelector('.choiseimg');
+    
+    if (buttonChoise && choiseimg) {
+      buttonChoise.addEventListener('click', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        choiseimg.classList.toggle('talisman');
+        });
+    }
 
+    const Buttonchoise = document.querySelector('.choisemeal');
+    const choiseimgg = document.querySelector('.choiseimgg');
+    
+    if (Buttonchoise && choiseimgg) {
+      Buttonchoise.addEventListener('click', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        choiseimgg.classList.toggle('yourmeal');
+        });
+    }
+  
+    const slides = document.querySelectorAll('.slide');
+    let currentIndex = 2;
+    
+    function updateSlides() {
+    slides.forEach((slide, index) => {
+            
+    slide.className = 'slide';
+    slide.style.opacity = '';
+    slide.style.pointerEvents = '';
+            
+    const diff = index - currentIndex;
+            
+    if (Math.abs(diff) <= 2) {
+    if (diff === 0) {
+    slide.classList.add('active');
+    } else if (diff === -1) {
+    slide.classList.add('prev-1');
+    } else if (diff === 1) {
+    slide.classList.add('next-1');
+    } else if (diff === -2) {
+    slide.classList.add('prev-2');
+    } else if (diff === 2) {
+    slide.classList.add('next-2');
+    }
+    } else {
+    slide.style.opacity = '0';
+    slide.style.pointerEvents = 'none';
+    }
+    });
+    }
+    
+  slides.forEach(slide => {
+  slide.addEventListener('click', function(event) {
+    if (event.target.closest('.choiseButton')) return;
+            
+  const clickedIndex = parseInt(this.getAttribute('data-index'));
+    if (!isNaN(clickedIndex) && clickedIndex !== currentIndex) {
+    currentIndex = clickedIndex;
+    updateSlides();
+    }
+    });
+    });
+    
+    updateSlides();
+    window.addEventListener('resize', function() {
+        requestAnimationFrame(updateSlides);
+    });
 
-  const slides = document.querySelectorAll('.slide');
-  let currentIndex = 2;
   
-  function updateSlides() {
-  slides.forEach((slide, index) => {
-          
-  slide.className = 'slide';
-  slide.style.opacity = '';
-  slide.style.pointerEvents = '';
-          
-  const diff = index - currentIndex;
-          
-  
-  if (Math.abs(diff) <= 2) {
-  if (diff === 0) {
-  slide.classList.add('active');
-  } else if (diff === -1) {
-  slide.classList.add('prev-1');
-  } else if (diff === 1) {
-  slide.classList.add('next-1');
-  } else if (diff === -2) {
-  slide.classList.add('prev-2');
-  } else if (diff === 2) {
-  slide.classList.add('next-2');
-  }
-  } else {
-  slide.style.opacity = '0';
-  slide.style.pointerEvents = 'none';
-  }
-  });
-  }
-  
+  const dragzone = document.getElementById('dragzone');
+  let currentDraggedElement = null;
+  let offsetX, offsetY;
 
-slides.forEach(slide => {
-slide.addEventListener('click', function(e) {
-  if (e.target.closest('.choiseButton')) return;
-          
-const clickedIndex = parseInt(this.getAttribute('data-index'));
-  if (!isNaN(clickedIndex) && clickedIndex !== currentIndex) {
-  currentIndex = clickedIndex;
-  updateSlides();
-  }
-  });
-  });
-  
+  document.querySelectorAll('.draggable').forEach(item => {
+    // Начало перетаскивания
+    item.addEventListener('dragstart', function(event) {
+      // Сохраняем ссылку на сам элемент
+      currentDraggedElement = this;
+      event.dataTransfer.setData('text/uri-list', currentDraggedElement.src);
+      event.dataTransfer.setData('text/plain', currentDraggedElement.id);
+      event.dataTransfer.effectAllowed = 'move';
+      const rect = event.target.getBoundingClientRect();
+      offsetX = event.clientX - rect.left;
+      offsetY = event.clientY - rect.top;
+      // Визуальный эффект
+      this.style.opacity = '0.7';
+      this.style.zIndex = '1000';
+      // Сохраняем элемент в дататрансфер
+      event.dataTransfer.setDragImage(this, 0, 0);
+    });
 
-  updateSlides();
-  
-
-  window.addEventListener('resize', function() {
-      requestAnimationFrame(updateSlides);
+    item.addEventListener ('dragend', (event) => {
+      currentDraggedElement.style.opacity = '1';
+      currentDraggedElement.style.zIndex = '1000';
+      currentDraggedElement = null;
   });
 });
-  
-  function animIMG() {
-    let images = document.querySelectorAll('.struct_arch img')
-  
-    for (let i = 0; i < images.length; i++) {
-      images[i].style.cssText = `animation-delay: ${-i}s;`
+
+  document.addEventListener('dragover', function(event) {
+    event.preventDefault();
+    if (currentDraggedElement) {
+      event.dataTransfer.dropEffect = 'move';
     }
-  }
+  });
+
+  document.addEventListener ('drop', (event) => {
+    event.preventDefault();
+    if (!currentDraggedElement) return;
+    const imgUrl = event.dataTransfer.getData('text/uri-list');
+    const newLeftVw = pxToVw(event.clientX - offsetX);
+    const newTopVh = (event.clientY - offsetY) / window.innerHeight * 100 +  394;
+    currentDraggedElement.style.left = `${newLeftVw}vw`;
+    currentDraggedElement.style.top = `${newTopVh}vh`;
+    console.log('изображение помещено', imgUrl);
+    console.log(`Изображение перемещено в: ${event.clientX}, ${event.clientY}`);
+  });
+
+  window.addEventListener('resize', function() {
+    // Можно добавить логику для корректировки позиций при ресайзе
+    console.log('Размер окна изменен');
+  });
+
+  const makeButton = document.getElementById('makeButton');
+  const wishText = document.querySelector('.wish');
+  
+  makeButton.addEventListener('click', function() {
+      wishText.innerHTML = '<span class="wish-fulfilled">Желание сбудется</span>';
+  });
   
 
-
+  });
+  
+  // Функции:
+  //Функция для анимации
+    function animIMG() {
+      let images = document.querySelectorAll('.struct_arch img')
+    
+      for (let i = 0; i < images.length; i++) {
+        images[i].style.cssText = `animation-delay: ${-i}s;`
+      }
+    }
+  
+    // Функция для перевода пикселей в vw
+    function pxToVw(px) {
+      return (px / window.innerWidth) * 100;
+    }
+  
+    // Функция для перевода vw в пиксели
+    function vwToPx(vw) {
+      return (vw * window.innerWidth) / 100;
+    }
   
